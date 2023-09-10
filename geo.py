1f241f13cd445c5ae2e828235f658aaab9ecf405
_s4__s4___ <135080071+Matezin12@users.noreply.github.com>
2023-09-10 16:50:09 -0300

    Z3Z G30
    Localizador de ips simples em python

--- geo.py
+++ geo.py
@@ -1,23 +1,288 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #print("estale os pacotes a seguir e depois volte  ")
 #print("pip install requests,pkg python")
 
 import os 
 import requests
+import time
 
+def progress_bar(done):
+    print("\rProgress: [{0:50s}] {1:.1f}%".format('#' * int(done * 50), done * 100),end='')
+
+def test():
+    for n in range(101):
+        progress_bar(n/100)
+        time.sleep(1)
+
+
+starting ()      
+Progress: [####################
+time.sleep("3")
+os.system("clear") 
+time.sleep("2")    
+print("         Z3Z G30         ")
+print("                          ")
+print("░░░░░░███████ ]▄▄▄▄▄▄▄▄")
+print("▂▄▅█████████▅▄▃▂")
+print("I███████████████████]")
+print("◥⊙▲⊙▲⊙▲⊙▲⊙▲⊙▲⊙◤...")
+print("                          ")    
+time.sleep("6")
 def get_ip_details(ip):
     url = f"http://ip-api.com/json/{ip}"
     response = requests.get(url)
     data = response.json()
     if data["status"] == "success":
-        print("Detalhes do IP:")
-        print(f"País: {data['country']}")
-        print(f"Estado: {data['regionName']}")
-        print(f"Provedor de Internet: {data['isp']}")
+        print(" \033[31m            INFORMACOES OBTIDAS DO IP  \033[31m ")
+        print(f" \033[32m País: {data['country']} \033[32m ")
+        print(f" \033[33m Estado: {data['regionName']} \033[33m ")
+        print(f" \033[036m Provedor de Internet: {data['isp']} \033[036m ")
     else:
-        print("Erro ao obter detalhes do IP.")
+        print(" \033[31m Erro ao obter informações  do IP \033[31m ")
 
 def main():
-    ip = input("Digite o IP: ")
+    ip = input(" \033[036m Digite o IP: \033[31m ")
     get_ip_details(ip)
 
 if __name__ == "__main__":
